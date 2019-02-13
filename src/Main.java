2b1894cf89d653af7579a07f96d84d5b8e509fd5
Andrii Vasin <andriivasin@gmail.com>
2019-02-13 13:43:41 +0200

    com mes2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello GitHub!");
+        for (int i=0;i<10; i++){
+            System.out.print(i);
+        }
     }
 }
