f5bd102d3d4df80b3b4764225c298ba0337097d7
Andrii Vasin <andriivasin@gmail.com>
2019-02-15 05:34:31 +0200

    com mes3 brn

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-                /*
-        System.out.println("Hello GitHub!");
-        for (int i=0;i<10; i++){
-            System.out.println(i);
-        }
-        */
+        
 
         Queue LongQ = new Queue(100);
         Queue ShortQ = new Queue(4);
