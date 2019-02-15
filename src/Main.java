8bb24b6d333f3b8a8ba9cc95b01d96a2ddfe6803
Andrii Vasin <andriivasin@gmail.com>
2019-02-15 05:24:58 +0200

    com mes3 rvs

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
-
-                /*
-        System.out.println("Hello GitHub!");
-        for (int i=0;i<10; i++){
-            System.out.println(i);
-        }
-        */
-
-        Queue LongQ = new Queue(100);
-        Queue ShortQ = new Queue(4);
-        char ch;
-        int i;
-
-        System.out.println("LongQ for some ABC shit");
-
-        for (i = 0; i < 26; i++)
-            LongQ.put((char)('A' + i));
+    puA' + i));
 
         System.out.println("And showing what LongQ got at this moment:");
 
