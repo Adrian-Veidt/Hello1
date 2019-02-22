a0d2536332dbc656a8917f9472acf4503d1bb571
Andrii Vasin <andriivasin@gmail.com>
2019-02-22 23:57:28 +0200

    pyramid

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,11 @@
-import java.util.Scanner;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
+        Pyramid pyr1 = new Pyramid();
+        pyr1.test();
 
-        //Taking noOfRows value from the user
 
-        System.out.println("How Many Rows You Want In Your Pyramid?");
-
-        int noOfRows = sc.nextInt();
-
-        //Initializing rowCount with 1
-
-        int rowCount = 1;
-
-        System.out.println("Here Is Your Pyramid");
-
-        //Implementing the logic
-
-        for (int i = noOfRows; i >= 1; i--)
-        {
-            //Printing i*2 spaces at the beginning of each row
-
-            for (int j = 1; j <= i*2; j++)
-            {
-                System.out.print(" ");
-            }
-
-            //Printing j where j value will be from i to noOfRows
-
-            for (int j = i; j <= noOfRows; j++)
-            {
-                System.out.print(j+" ");
-            }
-
-            //Printing j where j value will be from noOfRows-1 to i
-
-            for (int j = noOfRows-1; j >= i; j--)
-            {
-                System.out.print(j+" ");
-            }
-
-            System.out.println();
-
-            //Incrementing the rowCount
-
-            rowCount++;
-        }
     }
 }
